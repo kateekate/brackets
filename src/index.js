14ae4804cb4f8542cf70c6a3d47ddc80f4cfdc52
Katya <katya.mikhaylova.98@list.ru>
2022-11-04 13:38:53 +0300

    the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const regStr = bracketsConfig.map(brackets => brackets.map(br => br.replace(/([\$\^\.\*\+\?\\\/\{\}\[\]\(\)\|!])/g, '\\$1')).join('')).join('|')
+  const reg = new RegExp(regStr)
+  console.log({ reg })
+  if (str.length % 2 != 0) return false
+  const bracketsRegex = /\(\)|\[\]|\{\}|\|\|/
+  while (reg.test(str)) str = str.replace(reg, '')
+  console.log({ str })
+  return (str === '') ? true : false
 }
